afae25c23fe1a43aef281a8f41ee6ab08be5b377
Shawn <sbrya025@odu.edu>
2023-11-08 11:41:21 -0500

    Start Thread for Production/Consumer

--- Producer-Consumer/processSynchronization.cpp
+++ Producer-Consumer/processSynchronization.cpp
@@ -1,14 +1,32 @@
 #include <iostream>
 #include <fstream>
-#include <queue>
-#include <vector>
-#include <functional>
-#include <algorithm>
+#include <pthread.h>
+#include <stdio.h>
 
 using namespace std;
 
+void * ThreadFunction(void *arguments)
+{
+    printf("Thread is running\n");
+    return NULL;
+}
+
 int main()
 {
+
+    pthread_t threadOne;
+
+    // Get Command line arguments
+    // Initialize buffer
+    // Create Producer Threads
+    // Create Consumer Threads
+    // Sleep
+    // Exit
+
+    printf("Calling thread\n");
+
+    //pthread_create(&threadOne, NULL, ThreadFunction, NULL);
+
 
     return 0;
 
